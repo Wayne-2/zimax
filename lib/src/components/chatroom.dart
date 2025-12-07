e6232b72c7db2c1507183b475aa71949c6bf87c8
wayne <fredmon667@gmail.com>
2025-12-07 16:40:45 +0100

    animated home page

--- lib/src/components/chatroom.dart
+++ lib/src/components/chatroom.dart
@@ -29,37 +29,48 @@ class _ChatPageState extends State<Chatroom> {
         backgroundColor: Colors.white,
         elevation: 0,
         automaticallyImplyLeading: false,
-        leading: const Icon(Icons.chevron_left, color: Colors.black),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(Icons.chevron_left, color: Colors.black),
+        ),
 
         title: Row(
           children: [
             CircleAvatar(
               radius: 16,
-              backgroundImage:
-                  NetworkImage("https://i.pravatar.cc/200?u=${widget.username}"),
+              backgroundImage: NetworkImage(
+                "https://i.pravatar.cc/200?u=${widget.username}",
+              ),
             ),
-
+        
             const SizedBox(width: 10),
-
+        
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   widget.username,
                   style: GoogleFonts.poppins(
-                      fontSize: 15, fontWeight: FontWeight.w600),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
                 Text(
                   "Active now",
-                  style: GoogleFonts.poppins(fontSize: 11, color: Colors.green),
-                )
+                  style: GoogleFonts.poppins(
+                    fontSize: 11,
+                    color: Colors.green,
+                  ),
+                ),
               ],
-            )
+            ),
           ],
         ),
 
         actions: const [
-          Icon(Icons.call, color: Colors.black),
+          Icon(Icons.call, color: Colors.black, size: 16),
           SizedBox(width: 12),
           Icon(Icons.more_vert, color: Colors.black),
           SizedBox(width: 12),
@@ -76,25 +87,31 @@ class _ChatPageState extends State<Chatroom> {
                 final msg = messages[i];
 
                 return Align(
-                  alignment:
-                      msg["fromMe"] ? Alignment.centerRight : Alignment.centerLeft,
+                  alignment: msg["fromMe"]
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
                   child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 14,
+                      vertical: 8,
+                    ),
                     margin: const EdgeInsets.only(bottom: 8),
                     constraints: const BoxConstraints(maxWidth: 250),
 
                     decoration: BoxDecoration(
-                      color:
-                          msg["fromMe"] ? const Color.fromARGB(255, 29, 29, 29) : Colors.grey.shade200,
+                      color: msg["fromMe"]
+                          ? const Color.fromARGB(255, 29, 29, 29)
+                          : Colors.grey.shade200,
                       borderRadius: BorderRadius.circular(16),
                     ),
 
                     child: Text(
-                      msg["text"] ,
+                      msg["text"],
                       style: GoogleFonts.poppins(
                         fontSize: 12,
-                        color: msg["fromMe"] ? const Color.fromARGB(255, 255, 255, 255) : const Color.fromARGB(255, 0, 0, 0),
+                        color: msg["fromMe"]
+                            ? const Color.fromARGB(255, 255, 255, 255)
+                            : const Color.fromARGB(255, 0, 0, 0),
                       ),
                     ),
                   ),
@@ -107,19 +124,19 @@ class _ChatPageState extends State<Chatroom> {
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-          
+
             decoration: BoxDecoration(
               color: const Color.fromARGB(255, 218, 218, 218),
               borderRadius: BorderRadius.circular(24),
               border: Border.all(color: Colors.black12),
             ),
-          
+
             child: Row(
               children: [
                 Icon(Icons.insert_emoticon, color: Colors.black45, size: 20),
-          
+
                 const SizedBox(width: 8),
-          
+
                 Expanded(
                   child: TextField(
                     controller: controller,
@@ -127,27 +144,27 @@ class _ChatPageState extends State<Chatroom> {
                       fontSize: 14,
                       color: Colors.black87,
                     ),
-          
+
                     decoration: InputDecoration(
                       hintText: "Message...",
                       hintStyle: GoogleFonts.poppins(
                         color: Colors.black38,
                         fontSize: 14,
                       ),
-          
+
                       border: InputBorder.none,
                       isDense: true,
                       contentPadding: EdgeInsets.zero,
                     ),
                   ),
                 ),
-          
+
                 const SizedBox(width: 6),
-          
+
                 GestureDetector(
                   onTap: () {
                     if (controller.text.trim().isEmpty) return;
-          
+
                     setState(() {
                       messages.add({
                         "fromMe": true,
@@ -156,17 +173,16 @@ class _ChatPageState extends State<Chatroom> {
                       controller.clear();
                     });
                   },
-          
+
                   child: const Icon(
                     Icons.arrow_upward,
                     color: Color.fromARGB(255, 0, 0, 0),
                     size: 18,
                   ),
-                )
+                ),
               ],
             ),
-          )
-
+          ),
         ],
       ),
     );
--- lib/src/components/imaget.dart
+++ lib/src/components/imaget.dart
@@ -1,69 +1,167 @@
 import 'package:flutter/material.dart';
 
 
-class ImageDisplay extends StatelessWidget {
+class ImageDisplay extends StatefulWidget {
   const ImageDisplay({super.key});
 
   @override
+  State<ImageDisplay> createState() => _ImageDisplayState();
+}
+
+class _ImageDisplayState extends State<ImageDisplay> with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+
+  late Animation<double> slide1;
+  late Animation<double> slide2;
+  late Animation<double> slide3;
+  late Animation<double> slide4;
+  late Animation<double> slideCenter;
+
+  late Animation<double> fade;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+
+    const curve = Curves.easeInOutCubic;
+
+    // 🎯 New shorter slide distances
+    slide1 = Tween<double>(begin: 35, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: curve),
+    );
+
+    slide2 = Tween<double>(begin: 30, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: curve),
+    );
+
+    slide3 = Tween<double>(begin: 40, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: curve),
+    );
+
+    slide4 = Tween<double>(begin: 30, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: curve),
+    );
+
+    slideCenter = Tween<double>(begin: 45, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: curve),
+    );
+
+    // 🎨 Fade 0 → 1
+    fade = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: curve),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      width: double.infinity,
-      height: 130,
-      child: Stack(
-        children: [
-          // Image 1 from the left
-          Positioned(
-            top: 25,
-            bottom: 25,
-            left: 0,
-            child: Imaget(
-              width: 80, 
-              height: 83, 
-              imagename:'assets/images/img3.png')),
-          // Image 1 from the right
-          Positioned(
-            top: 25,
-            bottom: 25,
-            right: 0,
-            child: Imaget(
-              width: 80, 
-              height: 83, 
-              imagename:'assets/images/img6.png')),
-          // Image 2 from the left
-          Positioned(
-            top: 18,
-            bottom: 18,
-            left: 40,
-            child: Imaget(
-              width: 100, 
-              height: 95, 
-              imagename:'assets/images/img5.png')),
-          // Image 2 from the right
-          Positioned(
-            top: 18,
-            bottom: 18,
-            right: 40,
-            child: Imaget(
-              width: 100, 
-              height: 95, 
-              imagename:'assets/images/img4.png')),
-          // Centered Image
-          Positioned(
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Imaget(
-                width: 135, 
-                height: 130, 
-                imagename:'assets/images/img2.png'),
-            )),
-     
-          
-        ],
-      ),
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (_, __) {
+        return Opacity(
+          opacity: fade.value, // fade entire stack smoothly
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            width: double.infinity,
+            height: 130,
+            child: Stack(
+              children: [
+
+                // Image 1 left
+                Positioned(
+                  top: 25,
+                  bottom: 25,
+                  left: 0,
+                  child: Transform.translate(
+                    offset: Offset(0, slide1.value),
+                    child: Imaget(
+                      width: 80,
+                      height: 83,
+                      imagename: 'assets/images/img3.png',
+                    ),
+                  ),
+                ),
+
+                // Image 1 right
+                Positioned(
+                  top: 25,
+                  bottom: 25,
+                  right: 0,
+                  child: Transform.translate(
+                    offset: Offset(0, slide2.value),
+                    child: Imaget(
+                      width: 80,
+                      height: 83,
+                      imagename: 'assets/images/img6.png',
+                    ),
+                  ),
+                ),
+
+                // Image 2 left
+                Positioned(
+                  top: 18,
+                  bottom: 18,
+                  left: 40,
+                  child: Transform.translate(
+                    offset: Offset(0, slide3.value),
+                    child: Imaget(
+                      width: 100,
+                      height: 95,
+                      imagename: 'assets/images/img5.png',
+                    ),
+                  ),
+                ),
+
+                // Image 2 right
+                Positioned(
+                  top: 18,
+                  bottom: 18,
+                  right: 40,
+                  child: Transform.translate(
+                    offset: Offset(0, slide4.value),
+                    child: Imaget(
+                      width: 100,
+                      height: 95,
+                      imagename: 'assets/images/img4.png',
+                    ),
+                  ),
+                ),
+
+                // Center image
+                Positioned(
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Transform.translate(
+                    offset: Offset(0, slideCenter.value),
+                    child: Center(
+                      child: Imaget(
+                        width: 135,
+                        height: 130,
+                        imagename: 'assets/images/img2.png',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
